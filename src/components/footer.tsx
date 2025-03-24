50e3dac7f2516faa89714612c3d5da01d8ed0e06
Rainnny7 <braydonrainnny@gmail.com>
2025-03-24 00:26:23 -0400

    make the footer mobile responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { ReactElement } from "react";
 
 const Footer = (): ReactElement => (
-    <footer className="absolute inset-x-0 bottom-0 mx-[calc(10%+1rem)] h-14 flex gap-1 items-center text-sm text-muted-foreground bg-[#0E0E11]">
+    <footer className="absolute inset-x-0 bottom-0 px-5 sm:px-0 sm:mx-[calc(10%+1rem)] h-14 flex gap-1 items-center text-sm text-muted-foreground bg-[#0E0E11] border-t sm:border-none border-dotted border-grid-line">
         Made with <span className="animate-pulse">❤️</span> by{" "}
         <Link
             className="text-primary hover:opacity-75 transition-opacity transform-gpu"
