ef4ed8aaf04cfecf3bcd6d82e3604dd85ad4228c
Rainnny7 <braydonrainnny@gmail.com>
2025-03-24 01:10:13 -0400

    include the copyright date in the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import Link from "next/link";
 import { ReactElement } from "react";
 
 const Footer = (): ReactElement => (
     <footer className="absolute inset-x-0 bottom-0 px-5 sm:px-0 sm:mx-[calc(10%+1rem)] h-14 flex gap-1 items-center text-sm text-muted-foreground bg-[#0E0E11] border-t sm:border-none border-dotted border-grid-line">
-        Made with <span className="animate-pulse">❤️</span> by{" "}
+        © {new Date().getFullYear()}{" "}
+        <span className="size-1 bg-muted-foreground/75 rounded-full" /> Made
+        with <span className="animate-pulse">❤️</span> by{" "}
         <Link
             className="text-primary hover:opacity-75 transition-opacity transform-gpu"
             href="https://github.com/Rainnny7"
