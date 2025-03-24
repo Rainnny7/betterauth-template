5a183fe89459aff0d1391b125c193d6c2267fbce
Rainnny7 <braydonrainnny@gmail.com>
2025-03-23 23:55:38 -0400

    make the sidebar more responsive

--- src/app/(pages)/dashboard/layout.tsx
+++ src/app/(pages)/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import { ReactElement, ReactNode } from "react";
 import DashboardNavbar from "~/components/dashboard/dashboard-navbar";
 import DashboardSidebar from "~/components/dashboard/dashboard-sidebar";
 import { SidebarInset, SidebarProvider } from "~/components/ui/sidebar";
+import { cn } from "~/lib/utils";
 
 const DashboardLayout = ({
     children,
@@ -9,7 +10,12 @@ const DashboardLayout = ({
     children: ReactNode;
 }): ReactElement => (
     <SidebarProvider>
-        <main className="px-[10%] pt-14 w-full min-h-screen flex">
+        <main
+            className={cn(
+                "sm:px-[10%] pt-14 w-full min-h-screen flex transition-all transform-gpu",
+                "[--sidebar-width:11.5rem] sm:[--sidebar-width:15rem]"
+            )}
+        >
             <DashboardNavbar />
             <DashboardSidebar />
             <SidebarInset className="ml-[var(--sidebar-width)] bg-transparent">
