baca0bf6f26a35dc941de9aa36c368e97be67510
Rainnny7 <braydonrainnny@gmail.com>
2025-03-24 01:21:42 -0400

    dont need this bg on the footer anymore

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { ReactElement } from "react";
 
 const Footer = (): ReactElement => (
-    <footer className="absolute inset-x-0 bottom-0 px-5 sm:px-0 sm:mx-[calc(10%+1rem)] h-14 flex gap-1 items-center text-sm text-muted-foreground bg-[#0E0E11] border-t sm:border-none border-dotted border-grid-line">
+    <footer className="absolute inset-x-0 bottom-0 px-5 sm:px-0 sm:mx-[calc(10%+1rem)] h-14 flex gap-1 items-center text-sm text-muted-foreground border-t sm:border-none border-dotted border-grid-line">
         © {new Date().getFullYear()}{" "}
         <span className="size-1 bg-muted-foreground/75 rounded-full" /> Made
         with <span className="animate-pulse">❤️</span> by{" "}
